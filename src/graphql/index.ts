0ff2f491c33b3f00f958e5e47760c2cdd10a2b6e
Mauricio Giuliani <mauricio_giuliani@dell.com>
2023-08-24 19:11:11 -0300

    wip

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,4 +1,4 @@
-import { makeExecutableSchema } from "@graphql-tools/schema";
+import { makeExecutableSchema } from "graphql-tools";
 import { createHandler } from "graphql-http/lib/use/express";
 import { typeDefs } from "./schema";
 import { resolvers } from "./resolvers";
